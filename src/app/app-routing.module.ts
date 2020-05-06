fc47bc198a090fc4168f1a2ac407702f7d5f9244
Luis Fernando Espezua Aguilar <lespezua@pe.ibm.com>
2020-05-06 11:40:04 -0500

    [LE] page 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CatalogComponent } from './pages/catalog/catalog.component';
 import { ProductComponent } from './pages/product/product.component';
+import { Page404Component } from './errors/page404/page404.component';
 
 const routes: Routes = [
   { path: '', component: CatalogComponent },
   { path: 'catalog', component: CatalogComponent },
   { path: 'product', component: ProductComponent },
+  { path: 'page404', component: Page404Component },
 ];
 
 @NgModule({
